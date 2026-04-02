c629e01d3e4f548548a384899876c7449aab56c3
Breno Anselmo <114410982+brenoanselmo@users.noreply.github.com>
2026-04-02 11:04:17 -0300

    primeiro projeto em python
    hand_controls_mouse. Controlando o mouse com uma das mãos.

---
+++ projects/hand_controls_mouse.py
@@ -0,0 +1,98 @@
+import time
+import cv2
+import mediapipe as mp
+import math
+import pyautogui
+
+# Inicializa o módulo MediaPipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
+# Inicializa o módulo MediaPipe Drawing para desenhar pontos de referência
+mp_drawing = mp.solutions.drawing_utils
+
+#gesture time control
+click_start_time = None
+click_times = []
+click_cooldown = 0.5
+scroll_mode = False
+freeze_cursor = False
+
+screen_w, screen_h = pyautogui.size()
+prev_screen_x, prev_screen_y = 0,0
+
+# Abre um objeto de captura de vídeo (0 para a câmera padrão)
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        continue
+    # invertendo o video
+    frame = cv2.flip(frame, 1)
+    # Converte o frame para o formato RGB
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Processa o frame para detectar as mãos
+    results = hands.process(frame_rgb)
+
+    # Verifica se as mãos foram detectadas
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Desenha os pontos de referência no frame
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        # Get finger tip
+        thumb_tip = hand_landmarks.landmark[4]
+        index_tip = hand_landmarks.landmark[8]
+        middle_tip = hand_landmarks.landmark[12]
+        ring_tip = hand_landmarks.landmark[16]
+        pinky_tip = hand_landmarks.landmark[20]
+
+        fingers = [
+            1 if hand_landmarks.landmark[tip].y<hand_landmarks.landmark[tip-2].y else 0
+            for tip in [8,12,16,20]
+        ]
+
+        #distance btwn thump and index
+        dist = math.hypot(thumb_tip.x-index_tip.x, thumb_tip.y-index_tip.y)
+        click_times = []
+        if dist < 0.06:
+            if not freeze_cursor:
+                freeze_cursor = True
+                click_times.append(time.time())
+                # double click check
+                if len(click_times) >= 2 and click_times[-1] - click_times[-2] < 0.5:
+                    pyautogui.doubleClick()
+                    cv2.putText(frame, "Double Click", (10,50), cv2.FONT_HERSHEY_SIMPLEX,
+                                1, (0, 255, 255), 2)
+                    click_times=[]
+                else:
+                    pyautogui.click()
+                    cv2.putText(frame, "Single Click", (10,50), cv2.FONT_HERSHEY_SIMPLEX,
+                                1, (255, 255, 0), 2)
+        else:
+            if freeze_cursor:
+                time.sleep(0.1)
+            freeze_cursor = False
+
+        #move cursor by index_finger
+        if not freeze_cursor:
+            screen_x= int(index_tip.x * screen_w)
+            screen_y= int(index_tip.y * screen_h)
+            pyautogui.moveTo(screen_x, screen_y, duration=0.05)
+            prev_screen_x, prev_screen_y = screen_x, screen_y
+
+    # Exibe o quadro com os pontos de referência das mãos
+    cv2.imshow("Reconhecimento de Mãos", frame)
+    key = cv2.waitKey(10)
+    # Sai quando 'q' for pressionado
+    if key == 27:
+        break
+
+# Libera o objeto de captura de vídeo e fecha as janelas do OpenCV
+cap.release()
+cv2.destroyAllWindows()
